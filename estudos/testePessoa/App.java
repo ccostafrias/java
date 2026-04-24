78a7a7e745344086e3ebb9b88786912b41aab7f7
Caique Costa <ccostafrias@gmail.com>
2026-04-24 17:38:01 -0300

    feat: melhora classe Pessoa

--- estudos/testePessoa/App.java
+++ estudos/testePessoa/App.java
@@ -1,22 +1,31 @@
 class Pessoa {
-  int id;
-  String nome;
-  String endereco;
+  private int id;
+  private String nome;
+  private String endereco;
 
-  Pessoa(int id, String nome, String endereco) {
+  public Pessoa(int id, String nome, String endereco) {
     this.id = id;
     this.nome = nome;
     this.endereco = endereco;
   }
 
-  void imprimir() {
+  public void imprimir() {
     System.out.println("Pessoa: id = " + this.id + ", nome = " + this.nome + ", endereço = " + this.endereco);
+  }
+
+  public String getEndereco() {
+    return this.endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
   }
 }
 
 class TestePessoa {
   public static void main(String [] args) {
     Pessoa p1 = new Pessoa(5, "opaa", "heyyy");
+
     p1.imprimir();
   }
 }
